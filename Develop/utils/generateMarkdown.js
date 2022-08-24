54318043d024efa0b2de542d75bef7439ea3d2e0
Ruth Kim <ruthkim@Ruths-MacBook-Air.local>
2022-08-23 23:34:43 -0400

    finalized

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -5,13 +5,13 @@ function renderLicenseBadge(license) {
   if (license == 'None') {
     return '';
   } else if (license == 'MIT') {
-    return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
   } else if (license == 'Apache 2.0') {
-    return `![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
+    return `[![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
   } else if (license == 'GPL 3.0') {
-    return `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
   } else if (license == 'BSD 3') {
-    return `![License: BSD 3](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)`
+    return `[![License: BSD 3](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
   }
 }
 
