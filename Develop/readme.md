218e24f3b281ee43e03f75312e655c7db9da3dc8
Ruth Kim <ruthkim@Ruths-MacBook-Air.local>
2022-08-23 23:17:54 -0400

    tests working and committing everything before video recording

--- Develop/readme.md
+++ Develop/readme.md
@@ -1,23 +1,26 @@
 
-  # asdf
-  adsf
+  # asdf [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]
+  
+  # Description
+  asdf
 
   # Table of Contents 
-  * [Repository][#repository]
-  * [Package][#package]
-  * [License][#license]
-  * [Command][#command]
-  * [URL][#url]
+  * [Description](#description)
+  * [Repository](#repository)
+  * [Package](#package)
+  * [License](#license)
+  * [Command](#command)
+  * [URL](#url)
   
   # Repository
- https://github.com/asdf/asdf
+  https://github.com/asdf/asdf
   
   # Package 
-  asd
-
+  asdf
+  
   # License
-  * MIT
-   
+  GPL 3.0 Badge uses "![License: GPL v3]" + "https://img.shields.io/badge/License-GPLv3-blue.svg" + https://www.gnu.org/licenses/gpl-3.0
+
   # Command
   asdf
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -4,14 +4,14 @@
 function renderLicenseBadge(license) {
   if (license == 'None') {
     return '';
-  } else if (license == 'MIT'){
-    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`
-  } else if (license == 'Apache 2.0'){
-    return `[![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]`
-  } else if (license == 'GPL 3.0'){
-    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]`
-  } else if (license == 'BSD 3'){
-    return `[![License: BSD 3](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]`
+  } else if (license == 'MIT') {
+    return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  } else if (license == 'Apache 2.0') {
+    return `![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
+  } else if (license == 'GPL 3.0') {
+    return `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+  } else if (license == 'BSD 3') {
+    return `![License: BSD 3](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)`
   }
 }
 
@@ -20,13 +20,13 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {
   if (license == 'None') {
     return '';
-  } else if (license == 'MIT'){
+  } else if (license == 'MIT') {
     return 'https://opensource.org/licenses/MIT'
-  } else if (license == 'Apache 2.0'){
+  } else if (license == 'Apache 2.0') {
     return 'https://opensource.org/licenses/Apache-2.0'
-  } else if (license == 'GPL 3.0'){
+  } else if (license == 'GPL 3.0') {
     return 'https://www.gnu.org/licenses/gpl-3.0'
-  } else if (license == 'BSD 3'){
+  } else if (license == 'BSD 3') {
     return 'https://opensource.org/licenses/BSD-3-Clause'
   }
 }
@@ -34,26 +34,43 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  
+  if (license == 'None') {
+    return '';
+  } else if (license == 'MIT') {
+    return `MIT Badge uses "![License: MIT]" + "https://img.shields.io/badge/License-MIT-yellow.svg"`
+  } else if (license == 'Apache 2.0') {
+    return `Apache 2.0 Badge uses "![License: Apache 2.0]" + "https://img.shields.io/badge/License-Apache_2.0-blue.svg"`
+  } else if (license == 'GPL 3.0') {
+    return `GPL 3.0 Badge uses "![License: GPL v3]" + "https://img.shields.io/badge/License-GPLv3-blue.svg"`
+  } else if (license == 'BSD 3') {
+    return `BSD 3 Badge uses "![License: BSD 3]" + "https://img.shields.io/badge/License-BSD_3--Clause-blue.svg"`
+  }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
-  # ${data.project} ${renderLicenseBadge(license)}
+  # ${data.project} ${renderLicenseBadge(data.license)}
+  
+  # Description
   ${data.description}
 
   # Table of Contents 
-  * tbd
+  * [Description](#description)
+  * [Repository](#repository)
+  * [Package](#package)
+  * [License](#license)
+  * [Command](#command)
+  * [URL](#url)
   
   # Repository
- https://github.com/${data.github}/${data.repository}
+  https://github.com/${data.github}/${data.repository}
   
   # Package 
   ${data.package}
   
   # License
-  ${data.license}
+  ${renderLicenseSection(data.license)} + ${renderLicenseLink(data.license)}
 
   # Command
   ${data.test}
