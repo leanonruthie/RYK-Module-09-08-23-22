c9144281b9f7dc4ebde2fa220cd13083c6de9548
Ruth Kim <ruthkim@Ruths-MacBook-Air.local>
2022-08-23 20:30:35 -0400

    still trying to figure out license badge rendering portion

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -5,13 +5,13 @@ function renderLicenseBadge(license) {
   if (license == 'None') {
     return '';
   } else if (license == 'MIT'){
-    return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`
   } else if (license == 'Apache 2.0'){
-    return `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]`
+    return `[![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]`
   } else if (license == 'GPL 3.0'){
-    return ``
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]`
   } else if (license == 'BSD 3'){
-    return `![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]`
+    return `[![License: BSD 3](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]`
   }
 }
 
@@ -25,7 +25,7 @@ function renderLicenseLink(license) {
   } else if (license == 'Apache 2.0'){
     return 'https://opensource.org/licenses/Apache-2.0'
   } else if (license == 'GPL 3.0'){
-    return ``
+    return 'https://www.gnu.org/licenses/gpl-3.0'
   } else if (license == 'BSD 3'){
     return 'https://opensource.org/licenses/BSD-3-Clause'
   }
@@ -42,11 +42,8 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
-  # ${data.project}
+  # ${data.project} 
   ${data.description}
-  
-
-  ${renderLicenseBadge(license)} ${data.license}
 
   # Table of Contents 
   * tbd
